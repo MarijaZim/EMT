02f2e3b945518d754628c29a2ec147a63e5af473
Marija Zimbakovska <48930612+MarijaZim@users.noreply.github.com>
2021-04-09 21:10:18 +0200

    Delete src/main directory

--- src/main/java/com/example/emt_lab/Model/Enumeration/Category.java
+++
@@ -1,9 +0,0 @@
-package com.example.emt_lab.Model.Enumeration;
-
-public enum Category {
-    NOVEL, THRILER, HISTORY, FANTASY, BIOGRAPHY, CLASSICS, DRAMA;
-
-    public String getCategory(){
-        return name();
-    }
-}
--- src/main/java/com/example/emt_lab/Repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.emt_lab.Repository;
-
-import com.example.emt_lab.Model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author,Long> {
-}
